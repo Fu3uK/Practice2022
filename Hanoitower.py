2b3177d1f417d02cb8864020edc419d4ba0db124
Fu3uK <dencw123@mail.ru>
2022-07-20 02:14:13 +0300

    Обновление файла HanoiTower.py
    Написана функция tower_of_hanoi.

--- Hanoitower.py
+++ Hanoitower.py
@@ -1,9 +1,19 @@
 import sys
 
 
+def tower_of_hanoi(numbers, start, end):
+    if numbers == 1:
+        print(f"Переложить диск 1 со стержня-{start} на стержень-{end}")
+    else:
+        tower_of_hanoi(numbers - 1, start, 6 - start - end)
+        print(f"Переложить диск {numbers} со стержня-{start} на стержень-{end}")
+        tower_of_hanoi(numbers - 1, 6 - start - end, end)
+
+
 def except_hook(cls, exception, traceback):
    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
- print()
+    numbers = int(input("Введите количество дисков: "))
+    tower_of_hanoi(numbers, 1, 2)
