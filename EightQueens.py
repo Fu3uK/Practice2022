2c51e21da15ec1bd085bc341a092a1fb896ca248
Fu3uK <dencw123@mail.ru>
2022-07-20 01:44:05 +0300

    Обновление файла EightQueens.py
    Написана главная функция EightQueens.

--- EightQueens.py
+++ EightQueens.py
@@ -1,10 +1,33 @@
 import sys
 
 
+def EightQueens():
+    n = 8
+    x = []
+    y = []
+    for i in range(n):
+        new_x, new_y = [int(s) for s in input().split()]
+        x.append(new_x)
+        y.append(new_y)
+    correct = True
+    for i in range(n):
+        for j in range(i + 1, n):
+            if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
+                correct = False
+    if correct:
+        return 1
+    else:
+        return -1
+
 
 def except_hook(cls, exception, traceback):
    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
-    print()
+    print("Введите числа от 1 до 8")
+    result = EightQueens()
+    if result == 1:
+        print("Ферзи не бьют друг друга.")
+    else:
+        print("Ферзи бьют друг друга.")
